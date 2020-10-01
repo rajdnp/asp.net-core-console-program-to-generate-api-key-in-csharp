e0f50a3d7485b2dffd8e3128dfb7502420263a3d
rajesh.ccin001 <rajesh.ccin001@gmail.com>
2020-10-01 21:55:35 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var key = new byte[32];
+
+            using (var generator = RandomNumberGenerator.Create())
+            {
+                generator.GetBytes(key);
+            }
+            string apiKey = Convert.ToBase64String(key);
+            Console.WriteLine("Your API KEY IS :    " + apiKey);
+        }
+    }
+}
